9ba762e99d9d177c2b0cbd4804dbc276fda026d8
hugolbz <yugo@laposte.net>
2022-11-04 14:39:59 +0100

    Format

--- plugins/init.lua
+++ plugins/init.lua
@@ -34,10 +34,10 @@ return {
         --     end,
         -- },
         --
-        ["neovim/nvim-lspconfig"] = {
-            config = function()
-                require "plugins.configs.lspconfig"
-                require "custom.plugins.lspconfig"
-            end,
-        },
+    ["neovim/nvim-lspconfig"] = {
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.plugins.lspconfig"
+        end,
+    },
 }

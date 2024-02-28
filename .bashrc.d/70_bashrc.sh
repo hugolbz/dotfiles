a460af2792c503be51b30d269b90c3da7c941f60
hugolbz <yugo@laposte.net>
2024-02-28 13:31:33 +0100

    Fix keymap on Xorg

--- .bashrc.d/70_bashrc.sh
+++ .bashrc.d/70_bashrc.sh
@@ -2,5 +2,6 @@ export LANG=en_US.utf8
 export EDITOR=nvim
 
 # Remap caps lock to esc
+pidof Xorg && setxkbmap -option caps:escape
 pidof X && setxkbmap -option caps:escape
 # On wayland: go to gnome-tweak-tool > keyboard > ...

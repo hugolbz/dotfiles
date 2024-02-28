4cdfd5a1da63df9a52491e41daa51029583b990f
hugolbz <yugo@laposte.net>
2024-02-28 13:30:11 +0100

    increase default scrollback for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,9 +10,10 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
+-- Default shell
+config.default_prog = { '/bin/bash', '-l' }
 
--- For example, changing the color scheme:
+-- Change UI
 config.color_scheme = 'Gogh (Gogh)'
 
 config.window_padding = {
@@ -23,12 +24,11 @@ config.window_padding = {
 }
 config.font = wezterm.font 'FiraMono Nerd Font Mono'
 
--- Spawn bash
-config.default_prog = { '/bin/bash', '-l' }
-
 config.hide_tab_bar_if_only_one_tab = true
 config.force_reverse_video_cursor = true
 
+-- Scrolling
+config.scrollback_lines = 10000
 local act = wezterm.action
 config.keys = {
   { key = 'UpArrow', mods = 'SHIFT', action = act.ScrollToPrompt(-1) },
